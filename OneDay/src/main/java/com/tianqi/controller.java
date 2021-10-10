14cb85d88db39a734f281b406c1d749bbccf4668
root <2445292401@qq.com>
2021-10-10 14:34:51 +0800

    代码执行two提交

--- OneDay/src/main/java/com/tianqi/controller.java
+++ OneDay/src/main/java/com/tianqi/controller.java
@@ -7,5 +7,7 @@ package com.tianqi;
 public class controller {
     public static void main(String[] args) {
         System.out.println("====第一天java");
+        System.out.println("第二天");
+        System.out.println("第三天");
     }
 }
