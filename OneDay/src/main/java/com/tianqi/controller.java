b9733a5f99aef36b2158da88c40c995e565512b3
xiaomi <xiaomi@itcast.cn>
2021-10-10 13:34:37 +0800

    代码进行one提交

---
+++ OneDay/src/main/java/com/tianqi/controller.java
@@ -0,0 +1,11 @@
+package com.tianqi;
+
+/**
+ * @Author:wangwenhui
+ * @Date: 2021/10/10 13:27
+ */
+public class controller {
+    public static void main(String[] args) {
+        System.out.println("====第一天java");
+    }
+}
